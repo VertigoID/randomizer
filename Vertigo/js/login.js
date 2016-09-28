0ddf5cb58b3d0ac6ef4dd0892a2d17979609fc69
Fachry Reza K <aink.fachry@gmail.com>
2016-09-28 22:25:10 +0700

    UI_Randomizer

---
+++ Vertigo/js/login.js
@@ -0,0 +1,26 @@
+var attempt =3;
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	
+		if(username == "Formget" && password == "formget#123")
+		{
+			alert("login successfuly");
+			window.location = "success.html";
+			return false;
+		}
+		else
+		{
+			attempt --;
+			alert("you have left "+attempt+" attempt;");
+			
+			if(attempt == 0)
+			{
+				document.getElementById("username").disabled = true;
+				document.getElementById("password").disabled = true;
+				document.getElementById("submit").disabled = true;
+				
+			}
+		}
+				
+}
---
+++ Vertigo/www/js/vertigocontrollers.js
@@ -0,0 +1,58 @@
+angular.module('vertigo.controllers', [])
+/* Login service */
+.service('LoginService', function($q) {
+    return {
+        loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+            if (name == 'user' && pw == 'secret') {
+                deferred.resolve();
+            } else {
+                deferred.reject();
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+})
+.controller('LoginCtrl', function($scope, LoginService, $location, $ionicPopup, $state) {
+    $scope.data = {};
+	
+    $scope.login = function() {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('tab.dash');
+        })
+
+		.success(function(data){
+			var alertPopup = $ionicPopup.alert({
+				title: 'Login Success!',
+				template: 'Welcome ma man!',
+			})
+		})
+		.error(function(data) {
+               alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Salah username atau password boh!'
+            });
+        });
+    }
+})
+
+/* FK IT, dont work on redirecting */
+.controller('RegisterCtrl', function ($scope, $location) {
+
+    $scope.register = function() {
+      $location.path('/templates/register');
+    };
+  });
+
+
+
